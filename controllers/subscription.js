3e47c181dfca9b1a43706070c57c1cdbb73d0f1b
Michael Mitrakos <mitrakmt@Michaels-MBP.fios-router.home>
2018-06-14 12:58:36 -0700

    feat: update function to save and charge users

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -7,21 +7,20 @@ let stripe = require('stripe')(process.env.STRIPE_KEY)
 subscriptionController.GET_SUBSCRIPTION_PLANS = (req, res) => {}
 
 subscriptionController.CREATE_NEW_PLAN = (req, res) => {
-  return stripe.plans.create(
-    {
-      product: 'subscription',
+  return stripe.plans
+    .create({
+      product: 'prod_D37TbZZ1K0jtVL',
       nickname: 'Ooloo Subscription',
       currency: 'usd',
       interval: 'month',
       amount: 10000, // $10
-    },
-    plan => {
+    })
+    .then(plan => {
       console.log('plan', plan)
-      return {
+      res.status(200).send({
         plan,
-      }
-    },
-  )
+      })
+    })
 }
 
 subscriptionController.DELETE_PLAN = (req, res) => {}
@@ -29,31 +28,27 @@ subscriptionController.DELETE_PLAN = (req, res) => {}
 // CUSTOMERS
 subscriptionController.CREATE_CUSTOMER = (req, res) => {
   let userId = req.user.id
+  let cardToken = req.body.cardToken
 
   return userModel.GET_USER(userId).then(user => {
-    const customer = stripe.customers.create(
-      {
+    return stripe.customers
+      .create({
         email: user.email,
-        source: 'mobile',
-      },
-      customer => {
-        console.log('customer', customer)
-        res.status(200).send(customer)
-      },
-    )
+        source: cardToken,
+      })
+      .then(customer => {
+        // Charge the Customer instead of the card:
+        const charge = stripe.charges.create({
+          amount: 1000,
+          currency: 'usd',
+          customer: customer.id,
+        })
+
+        return subscriptionModel.SAVE_CUSTOMER_ID(userId, customer.id).then(customer => {
+          res.status(200).send({ customer })
+        })
+      })
   })
-  // Returns
-  // {
-  //   "id": "cus_4fdAW5ftNQow1a",
-  //   "object": "customer",
-  //   "account_balance": 0,
-  //   "created": 1528903495,
-  //   "currency": null,
-  //   ...
-  //   "livemode": false,
-  //   "email": "jenny.rosen@example.com",
-  //   ...
-  // }
 }
 
 subscriptionController.DELETE_CUSTOMER = (req, res) => {
@@ -62,28 +57,34 @@ subscriptionController.DELETE_CUSTOMER = (req, res) => {
 
 // SUBSCRIPTIONS
 subscriptionController.CREATE_USER_SUBSCRIPTION = (req, res) => {
-  return stripe.subscriptions.create({
-    customer: 'cus_4fdAW5ftNQow1a',
-    items: [{ plan: 'plan_CBXbz9i7AIOTzr' }],
-  })
+  return stripe.subscriptions
+    .create({
+      customer: 'cus_D37XHE0pDzwiSc',
+      items: [{ plan: 'plan_D37UJBYkjT1tyP' }],
+    })
+    .then(subscription => {
+      console.log('subscription', subscription)
+      res.status(200).send({
+        subscription,
+      })
+    })
 }
 
 subscriptionController.CANCEL_SUBSCRIPTION = (req, res) => {}
 
 // PRODUCTS
 subscriptionController.CREATE_NEW_PRODUCT = (req, res) => {
-  return stripe.products.create(
-    {
+  return stripe.products
+    .create({
       name: 'subscription',
-      type: 'subscription',
-    },
-    product => {
+      type: 'service',
+    })
+    .then(product => {
       console.log('product', product)
-      return {
+      res.status({
         product,
-      }
-    },
-  )
+      })
+    })
 }
 
 subscriptionController.DELETE_PRODUCT = (req, res) => {}
--- models/subscription.js
+++ models/subscription.js
@@ -1,3 +1,20 @@
 let subscriptionModel = {}
+let User = require('../db').Users
+
+subscriptionModel.SAVE_CUSTOMER_ID = (id, customerId) => {
+  return User.findOne({
+    where: {
+      id,
+    },
+  }).then(user => {
+    return user
+      .updateAttributes({
+        customerId,
+      })
+      .then(status => {
+        return status
+      })
+  })
+}
 
 module.exports = subscriptionModel
