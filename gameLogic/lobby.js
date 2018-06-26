74fba56d93004d4122855f4b7cebb6c342edf4e2
Corey Wolff <jesterswilde@gmail.com>
2018-06-26 10:41:56 -0700

    Uncommented old code now that interests are on players.

--- gameLogic/lobby.js
+++ gameLogic/lobby.js
@@ -44,7 +44,7 @@ const queueOrMatch = async(playerObj, {_startBotGameTimer = startBotGameTimer, _
 const botGame = async(playerObj)=>{
 	const questions = await getQuestions(playerObj.interests, [playerObj]); 
 	const gameObject = {
-			interests: ['foo', 'bar'],
+			interests: playerObj.interests,
 			players: [playerObj],
 			questions
 		};
@@ -56,10 +56,9 @@ const playerConnects = async(socket, token, {id, _queueOrMatch = queueOrMatch} =
 		if(error){
 			throw error; 
 		}
-		//TODO put back when interests are more of a thing
-		//const interests = await GET_INTERESTS(id); 
+		const interests = await GET_INTERESTS(id); 
+		console.log(interests); 
 		const {username} = await GET_USER(id); 
-		const interests = ['doctoring']; 
 		const playerObject = {socket, id, interests, username};
 		_queueOrMatch(playerObject); 
 		//botGame(playerObject); 
