73f38cd46ebf4a6009193b0d50c3ccc9d6a926a3
Corey Wolff <jesterswilde@gmail.com>
2018-06-27 13:15:31 -0700

    Start of bar graph

--- gameLogic/game/chart.js
+++ gameLogic/game/chart.js
@@ -4,11 +4,17 @@ const {Op} = require('Sequelize');
 const getBarChart = async(userId, interests)=>{
 	interestsIds = interests.map(({id})=> id); 
 	const answers = await Answers.find({
+		attributes:[
+   			[Sequelize.where(Sequelize.fn("COUNT", Sequelize.col("questions.correct")), {'questions.correct': true, "correct"],
+   			[Sequelize.where(Sequelize.fn("COUNT", Sequelize.col("questions.correct")), {'questions.correct': false, "incorrect"]
+		]
 		include:[{
 			model: Questions,
-			require: true, 
-			where:{[Op.overlap]:{topics: interestsIds}}
-		}]
+			require: true,
+			attributes:[]
+			where:{[Op.overlap]:{topics: interestsIds}, userId}
+		}],
+		group: 'questions.topics'
 	});
 	console.log(answer); 
 }
