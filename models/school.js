28f29952c53f3ed649ba365c648baa8327c6094c
Corey Wolff <wolff.corey@gmail.com>
2018-07-01 21:12:18 -0700

    Can now accept users without a university for leaderboards. (#47)

--- models/school.js
+++ models/school.js
@@ -17,9 +17,11 @@ schoolModel.GET_SCHOOLS = () => {
 
 schoolModel.GET_SCHOOL_RANK = async(name)=>{
   try{
-    pointsObj = await School.findOne({where:{name, isActive: true}, attributes:['points', 'name', 'name']});
-    const rank = await School.count({where: {points: {[Op.gt]: pointsObj.dataValues.points}}})
-    return {rank: rank + 1, name};
+  	if(name){
+	    pointsObj = await School.findOne({where:{name, isActive: true}, attributes:['points', 'name', 'name']});
+	    const rank = await School.count({where: {points: {[Op.gt]: pointsObj.dataValues.points}}})
+	    return {rank: rank + 1, name};
+  	}
   }catch(error){
     console.error('Error in get rank model', error);
   }
@@ -45,10 +47,12 @@ schoolModel.GET_SCHOOL_LEADERBOARD = async(name)=>{
 		const schoolRankePromise = schoolModel.GET_SCHOOL_RANK(name); 
 		const [topSchools, schoolRank] = await Promise.all([topSchoolsPromise, schoolRankePromise]);
 		const leaderboard = topSchools.map((user, index)=> ({...user, rank: index + 1}));
-		if(schoolRank.rank <= 10){
-			leaderboard[schoolRank.rank-1].isYourSchool = true; 
-		}else{
-			leaderboard.push({...schoolRank, isYourSchool:true})
+		if(schoolRank){
+			if(schoolRank.rank <= 10){
+				leaderboard[schoolRank.rank-1].isYourSchool = true; 
+			}else{
+				leaderboard.push({...schoolRank, isYourSchool:true})
+			}
 		}
 		return leaderboard;
 	}catch(error){
